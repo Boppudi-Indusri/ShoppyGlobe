c3c5f8a8f51df7b502d1d5c5709da766137eb7f5
boppudi-indusri <boppudiindusri681@gmail.com>
2025-11-25 11:54:15 +0530

    ProductItem added

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../store/cartSlice";
+import { Link } from "react-router-dom";
+
+export default function ProductItem({ product }) {
+  const dispatch = useDispatch();
+  const handleAdd = () => dispatch(addProduct(product));
+
+  return (
+    <div className="product-item">
+      <Link to={`/product/${product.id}`}>
+        <img src={product.thumbnail} alt={product.title} loading="lazy" style={{ width: "100%", height: 150, objectFit: "cover" }} />
+      </Link>
+      <h3>{product.title}</h3>
+      <p>₹ {product.price}</p>
+      <button onClick={handleAdd}>Add to Cart</button>
+      <Link to={`/product/${product.id}`}>View</Link>
+    </div>
+  );
+}
+
+ProductItem.propTypes = {
+  product: PropTypes.object.isRequired
+};
